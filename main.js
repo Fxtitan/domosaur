4250b8dfcb2989cc127c32a82c003a5225a76054
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-08-04 14:38:19 -0400

    done

--- main.js
+++ main.js
@@ -17,7 +17,33 @@ hiddenDino.parentNode.removeChild(hiddenDino);
 const tricera = document.querySelector('#triceratops');
 tricera.style.width = '324px';
 
-const makeBold = function() {
-    heading.fontWeight = 600;
-}
+const feathered = document.querySelector('#feathers');
+
+const togSwitch = document.querySelector('#toggle');
+
+const dinoRow = document.querySelector('#row');
+
+const bigDino = document.querySelector('#biggify');
+
+
+//EVENT LISTENERS/////////////////////////////////////////////////////////////
+// const makeOrange = function() {
+//     em3.style.color = 'orange';
+// }
+// em3.addEventListener('click', makeOrange);
+em3.addEventListener('click', () => em3.style.color = 'orange');
+
+tricera.addEventListener('click', () => tricera.style.border = 'thick solid red');
+
+feathered.addEventListener('click', () => feathered.style.opacity = '50%');
+
+togSwitch.addEventListener('click', () => dinoRow.style.backgroundColor = 'purple');
+
+bigDino.addEventListener('mouseover', () => bigDino.style.width = '200px');
+
+
+
+
+
+
 
